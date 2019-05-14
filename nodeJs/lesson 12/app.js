a2dfc44c17531a8e03f2882f82d8693f6e1f6e09
Cédric David <cedric.david@btopenworld.com>
2019-05-14 11:35:28 +0800

    Create app.js

---
+++ nodeJs/lesson 12/app.js
@@ -0,0 +1,10 @@
+const readline = require('readline');
+const fs = require('fs');
+
+const myInterface = readline.createInterface({
+  input: fs.createReadStream('shoppingList.txt')
+})
+const printData = (data) => {
+  console.log(`Item: ${data}`)
+}
+myInterface.on('line', printData)
