c38e7059e47ddf7f5f5e4e10c1894b1720865007
Cédric David <cedric.david@btopenworld.com>
2019-05-14 11:25:11 +0800

    Create app.js

---
+++ nodeJs/lesson 11/app.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+let secretWord = null;
+let readDataCallback = (err, data) => {
+  if (err) {
+    console.log(`Something went wrong: ${err}`);
+  } else {
+    console.log(`Provided file contained: ${data}`);
+  }
+}; 
+fs.readFile('finalFile.txt', 'utf-8', readDataCallback);
+secretWord = "cheeseburgerpizzabagels";
