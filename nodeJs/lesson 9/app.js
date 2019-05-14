e35b955bf0ef514bb0ab5a4eccd370866966c430
Cédric David <cedric.david@btopenworld.com>
2019-05-14 10:38:00 +0800

    Update app.js

--- nodeJs/lesson 9/app.js
+++ nodeJs/lesson 9/app.js
@@ -1,20 +1,10 @@
-let secretValue = Math.floor(1+Math.random()*10).toString();
+let {testNumber} = require('./game.js');
 
-let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+process.stdout.write("I'm thinking of a number from 1 through 10. What do you think it is? \n(Write \"quit\" to give up.)\n\nIs the number ... ");
 
-module.exports = {
-  testNumber: (input) => {
-   if (input === 'quit') {
-    process.stdout.write('Ok. Bye!\n')
-    process.exit();
-  }
-  if (!numbers.includes(input)) {
-    process.stdout.write('Choose a number from 1 through 10!\nIs the number ... ')
-  } else if (input === secretValue) {
-    process.stdout.write('Woah you got it! Are you psychic? See you later!\n')
-    process.exit();
-  } else {
-    process.stdout.write("Nope. Guess again!\nIs the number ... ");
-  }
-  }
-}
+let playGame = (userInput) => {
+  let input = userInput.toString().trim();
+	testNumber(input);
+};
+
+process.stdin.on('data', playGame)
